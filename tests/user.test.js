0f559cea22e28a8e03f9cc8aa370c7654cbb0c8d
boom <linjieyang77@gmail.com>
2019-05-30 12:19:50 +0800

    添加task测试组件

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,24 +1,9 @@
 const request = require('supertest')
-const jwt = require('jsonwebtoken')
-const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
+const { userOneId, userOne, setupDatabase } = require('./fixtures/db')
 
-const userOneId = new mongoose.Types.ObjectId()
-const userOne = {
-    _id: userOneId,
-    name: 'foo',
-    email: 'foo@example.com',
-    password: '56what!!',
-    tokens: [{
-        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
-    }]
-}
-
-beforeEach(async () => {
-    await User.deleteMany()
-    await new User(userOne).save()
-})
+beforeEach(setupDatabase)
 
 test('Should signup a new user', async () => {
     const response = await request(app).post('/users').send({
@@ -105,11 +90,11 @@ test('Should update valid user fields', async () => {
         .patch('/users/me')
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send({
-            name: 'yoo'
+            name: 'Jess'
         })
         .expect(200)
     const user = await User.findById(userOneId)
-    expect(user.name).toEqual('yoo')
+    expect(user.name).toEqual('Jess')
 })
 
 test('Should not update invalid user fields', async () => {
